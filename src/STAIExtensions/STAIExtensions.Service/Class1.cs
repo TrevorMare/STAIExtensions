0151315702975966255ea72db7b1fc5b977e872c
Trevor Mare <trevorm336dev@gmail.com>
2021-12-19 14:30:58 +0200

    Queries and sub types

--- src/STAIExtensions/STAIExtensions.Service/Class1.cs
+++ src/STAIExtensions/STAIExtensions.Service/Class1.cs
@@ -1,5 +1,31 @@
-﻿namespace STAIExtensions.Service;
+﻿using STAIExtensions.Abstractions.Common;
+using STAIExtensions.Abstractions.Data;
+using STAIExtensions.Abstractions.DataContracts.Models;
+using STAIExtensions.Data.AzureDataExplorer;
+using STAIExtensions.Data.AzureDataExplorer.Queries;
+
+namespace STAIExtensions.Service;
 
 public class Class1
 {
+    
+    public void Scratch()
+    {
+
+        TelemetryLoader telemetryLoader = new STAIExtensions.Data.AzureDataExplorer.TelemetryLoader(new TelemetryLoaderOptions("abc", "cde"));
+
+        var specifiedQuery = new AzureDataExplorerQuery<Abstractions.DataContracts.Models.IAvailability>();
+
+        var tableName = AzureDataExplorerQueryFactory.GetDataContractSourceTableName(DataContractSource.Availability);
+        var query = AzureDataExplorerQueryFactory.BuildCustomQuery<IAvailability>(
+            tableName, tableName);
+
+        
+
+        var result = telemetryLoader.ExecuteQueryAsync(specifiedQuery).Result;
+
+
+
+    }
+    
 }
