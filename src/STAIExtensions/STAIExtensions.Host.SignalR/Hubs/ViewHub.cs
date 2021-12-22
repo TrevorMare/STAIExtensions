f2363f789784f308a83daf797198f8f86030648f
Trevor Mare <trevorm336dev@gmail.com>
2021-12-22 08:03:00 +0200

    Before Merge

--- src/STAIExtensions/STAIExtensions.Host.SignalR/Hubs/ViewHub.cs
+++ src/STAIExtensions/STAIExtensions.Host.SignalR/Hubs/ViewHub.cs
@@ -8,6 +8,7 @@ public class ViewHub : Hub
     public async Task SendMessage(string user, string message)
     {
         await Clients.All.SendAsync("ReceiveMessage", user, message);
+        
     }
     
     
