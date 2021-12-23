8515e454af371a908049677903950c2ef95268d6
Trevor Mare <trevorm336dev@gmail.com>
2021-12-23 11:33:48 +0200

    Initial Api Host Controllers

---
+++ src/STAIExtensions/STAIExtensions.Host.Api/Controllers/DataSetController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace STAIExtensions.Host.Api.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class DataSetController : ControllerBase
+{
+
+    [HttpGet]
+    [Route("ListDataSets")]
+    public IEnumerable<string> ListDataSets()
+    {
+
+        return null;
+
+    }
+
+}
