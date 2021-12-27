0d50339806500cc7567c9731eb77ceafea0c2072
Trevor Mare <trevorm336dev@gmail.com>
2021-12-27 17:37:02 +0200

    Sample view

--- src/STAIExtensions/STAIExtensions.Core/Collections/ViewCollection.cs
+++ src/STAIExtensions/STAIExtensions.Core/Collections/ViewCollection.cs
@@ -1,4 +1,6 @@
-﻿using STAIExtensions.Abstractions.Collections;
+﻿using Microsoft.Extensions.Logging;
+using STAIExtensions.Abstractions;
+using STAIExtensions.Abstractions.Collections;
 using STAIExtensions.Abstractions.Views;
 
 namespace STAIExtensions.Core.Collections;
@@ -8,8 +10,9 @@ public class ViewCollection : Abstractions.Collections.IViewCollection
 
     #region Members
 
-    private List<IDataSetView> _dataSetViewCollection = new();
-
+    private readonly List<IDataSetView> _dataSetViewCollection = new();
+    private readonly ILogger<ViewCollection>? _logger;
+    
     #endregion
 
     #region Properties
@@ -18,56 +21,92 @@ public class ViewCollection : Abstractions.Collections.IViewCollection
 
     #endregion
 
+    #region ctor
+
+    public ViewCollection()
+    {
+        _logger = DependencyExtensions.CreateLogger<ViewCollection>();
+    }
+
+    #endregion
+    
     #region Methods
     public IDataSetView? GetView(string id, string userSessionId)
     {
-        if (string.IsNullOrEmpty(id))
-            throw new ArgumentNullException(nameof(id));
-
-        if (UseStrictUserSession)
+        try
         {
-            if (string.IsNullOrEmpty(userSessionId))
-                throw new ArgumentNullException(nameof(userSessionId));
+            if (string.IsNullOrEmpty(id))
+                throw new ArgumentNullException(nameof(id));
 
-            return _dataSetViewCollection.FirstOrDefault(dsv =>
-                string.Equals(dsv.Id, id, StringComparison.CurrentCultureIgnoreCase) && string.Equals(dsv.OwnerId,
-                    userSessionId, StringComparison.CurrentCultureIgnoreCase));
-        }
+            if (UseStrictUserSession)
+            {
+                if (string.IsNullOrEmpty(userSessionId))
+                    throw new ArgumentNullException(nameof(userSessionId));
+
+                return _dataSetViewCollection.FirstOrDefault(dsv =>
+                    string.Equals(dsv.Id, id, StringComparison.CurrentCultureIgnoreCase) && string.Equals(dsv.OwnerId,
+                        userSessionId, StringComparison.CurrentCultureIgnoreCase));
+            }
         
-        return _dataSetViewCollection.FirstOrDefault(dsv => string.Equals(dsv.Id, id, StringComparison.CurrentCultureIgnoreCase));
+            return _dataSetViewCollection.FirstOrDefault(dsv => string.Equals(dsv.Id, id, StringComparison.CurrentCultureIgnoreCase));
+        }
+        catch (Exception e)
+        {
+            _logger?.LogError(e, "An error occured retrieving view. {Error}", e);
+            throw;
+        }
     }
 
     public IDataSetView CreateView(string viewTypeName, string ownerId)
     {
-        // Fully qualified type name
-        var type = Type.GetType(viewTypeName);
-
-        if (type == null)
-            throw new Exception($"Unable to create view from type {viewTypeName}, View type not found");
-
-        var resolvedInstance = Abstractions.DependencyExtensions.ServiceProvider?.GetService(type);
-        if (resolvedInstance == null)
-        {
-            resolvedInstance = Activator.CreateInstance(type);
-        }
         
-        if (resolvedInstance == null)
-            throw new Exception($"Unable to create view from type {viewTypeName}");
+        try
+        {
+            _logger?.LogInformation("Attempting to create view of type {ViewType} for owner {OwnerId}", 
+                viewTypeName, ownerId);
+            // Fully qualified type name
+            var type = Type.GetType(viewTypeName);
 
-        IDataSetView instance = (IDataSetView) resolvedInstance;
-        instance.OwnerId = ownerId;
+            if (type == null)
+                throw new Exception($"Unable to create view from type {viewTypeName}, View type not found");
 
-        this._dataSetViewCollection.Add(instance);
+            var resolvedInstance = Abstractions.DependencyExtensions.ServiceProvider?.GetService(type);
+            if (resolvedInstance == null)
+            {
+                resolvedInstance = Activator.CreateInstance(type);
+            }
+        
+            if (resolvedInstance == null)
+                throw new Exception($"Unable to create view from type {viewTypeName}");
+
+            IDataSetView instance = (IDataSetView) resolvedInstance;
+            instance.OwnerId = ownerId;
+
+            this._dataSetViewCollection.Add(instance);
     
-        return instance;
+            return instance;
+        }
+        catch (Exception e)
+        {
+            _logger?.LogError(e, "An error occured creating the view. {Error}", e);
+            throw;
+        }
     }
 
     public IDataSetView? GetViewForUpdate(string id)
     {
-        if (string.IsNullOrEmpty(id))
-            throw new ArgumentNullException(nameof(id));
+        try
+        {
+            if (string.IsNullOrEmpty(id))
+                throw new ArgumentNullException(nameof(id));
         
-        return _dataSetViewCollection.FirstOrDefault(dsv => string.Equals(dsv.Id, id, StringComparison.CurrentCultureIgnoreCase));
+            return _dataSetViewCollection.FirstOrDefault(dsv => string.Equals(dsv.Id, id, StringComparison.CurrentCultureIgnoreCase));
+        }
+        catch (Exception e)
+        {
+            _logger?.LogError(e, "An error occured retrieving view. {Error}", e);
+            throw;
+        }
     }
 
     #endregion
--- src/STAIExtensions/STAIExtensions.Host/Services/ServiceRunner.cs
+++ src/STAIExtensions/STAIExtensions.Host/Services/ServiceRunner.cs
@@ -19,17 +19,10 @@ public class ServiceRunner : IHostedService
     public Task StartAsync(CancellationToken cancellationToken)
     {
 
-        using (var scope = _serviceProvider.CreateScope())
-        {
-            
-              
-            var telemetryLoader = new STAIExtensions.Data.AzureDataExplorer.TelemetryLoader(new TelemetryLoaderOptions("2h1cqet6ae4nua4fjc0zqux14d2o1pb3uguavjts", "e666c38b-3ced-4bad-9661-ddc4dd01bb5b"));
-            _ds = new Core.DataSets.DataContractDataSet(telemetryLoader, new DataContractDataSetOptions(), "MyDataSet");
-
-            _ds.StartAutoRefresh(TimeSpan.FromSeconds(30), cancellationToken);
+        var telemetryLoader = new STAIExtensions.Data.AzureDataExplorer.TelemetryLoader(new TelemetryLoaderOptions("2h1cqet6ae4nua4fjc0zqux14d2o1pb3uguavjts", "e666c38b-3ced-4bad-9661-ddc4dd01bb5b"));
+        _ds = new Core.DataSets.DataContractDataSet(telemetryLoader, new DataContractDataSetOptions(), "MyDataSet");
+        _ds.StartAutoRefresh(TimeSpan.FromSeconds(30), cancellationToken);
         
-            var z = new  Core.DataSets.DataContractDataSet(telemetryLoader, new DataContractDataSetOptions(), "MyDataSet");
-        }
         
       
 
