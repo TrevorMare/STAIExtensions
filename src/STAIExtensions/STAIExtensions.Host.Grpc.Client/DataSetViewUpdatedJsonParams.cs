bf428c30b4a654232e720a0fe6dfcadb0efe7b71
Trevor Mare <trevorm336dev@gmail.com>
2022-01-05 17:25:09 +0200

    Explicitly pack nuget projects as solution build does not output all packages

--- src/STAIExtensions/STAIExtensions.Host.Grpc.Client/DataSetViewUpdatedJsonParams.cs
+++ src/STAIExtensions/STAIExtensions.Host.Grpc.Client/DataSetViewUpdatedJsonParams.cs
@@ -1,6 +1,6 @@
 ﻿namespace STAIExtensions.Host.Grpc.Client;
 
-public record class DataSetViewUpdatedJsonParams(string ViewId, string Payload)
+public record DataSetViewUpdatedJsonParams(string ViewId, string Payload)
 {
     public string ViewId { get; } = ViewId;
     public string Payload { get; } = Payload;
