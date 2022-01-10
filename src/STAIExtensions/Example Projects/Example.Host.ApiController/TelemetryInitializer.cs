9a7cd0d4093b4c375d8d2cf1507fa857063130dc
Trevor Mare <trevorm336dev@gmail.com>
2022-01-10 17:55:16 +0200

    Added example host Api Controller

---
+++ src/STAIExtensions/Example Projects/Example.Host.ApiController/TelemetryInitializer.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.ApplicationInsights.Channel;
+using Microsoft.ApplicationInsights.Extensibility;
+
+namespace Example.Host.ApiController;
+
+public class TelemetryInitializer : ITelemetryInitializer
+{
+    public void Initialize(ITelemetry telemetry)
+    {
+        telemetry.Context.Cloud.RoleName = "Host Api Controller";
+        telemetry.Context.InstrumentationKey = "c065e6f4-03cd-472e-94fd-c0518f8463f3";
+    }
+}
