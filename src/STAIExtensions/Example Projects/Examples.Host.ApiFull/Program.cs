c4c96b735b48d10d17aecb2f35af49e4b4a119f3
Trevor Mare <trevorm336dev@gmail.com>
2022-01-11 11:21:13 +0200

    Example of a Full Hosting Api

---
+++ src/STAIExtensions/Example Projects/Examples.Host.ApiFull/Program.cs
@@ -0,0 +1,76 @@
+using Examples.Host.ApiFull;
+using Examples.Host.ApiFull.Services;
+using Microsoft.ApplicationInsights.Extensibility;
+using STAIExtensions.Core;
+using STAIExtensions.Host.Api;
+using STAIExtensions.Host.Grpc;
+using STAIExtensions.Host.SignalR;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllers();
+
+// Inject the Controllers and the options to expose the Api 
+var controllerApiOptions = new ApiOptions()
+{
+    HeaderName = "x-api-key",
+    UseAuthorization = true,
+    AllowedApiKeys = new List<string>()
+    {
+        builder.Configuration["ApiAuthorizationToken"]
+    }
+};
+builder.Services.UseSTAIExtensionsApiHost(() => controllerApiOptions);
+
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+// Wire up the local application insights to generate data for the DataSets
+builder.Services.AddSingleton<ITelemetryInitializer, TelemetryInitializer>();
+builder.Services.AddApplicationInsightsTelemetry(builder.Configuration["ApplicationInsights:ConnectionString"]);
+ 
+// Register the STAIExtensions Core Library with the options provided
+var dataSetCollectionOptions = new STAIExtensions.Abstractions.Collections.DataSetCollectionOptions(
+    null, 50);
+var viewCollectionOptions = new STAIExtensions.Abstractions.Collections.ViewCollectionOptions(
+    1000, 
+    false, 
+    true, 
+    TimeSpan.FromMinutes(15));
+
+builder.Services.UseSTAIExtensions(() => dataSetCollectionOptions, () => viewCollectionOptions);
+
+// Now add the Grpc and the SignalR Communication services
+builder.Services.UseSTAISignalR();
+
+var grpcOptions = new GrpcHostOptions(true, builder.Configuration["GrpcAuthorizationToken"]);
+builder.Services.UseSTAIGrpc(grpcOptions);
+
+// Add the hosted service that will read the telemetry data
+builder.Services.AddHostedService<ServiceRunner>();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+// The Use Routing is required by the SignalR endpoint registration
+app.UseRouting();
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+// Use the Map the SignalR Hubs and Grpc Connections after the UseAuthorization call.
+app.MapSTAISignalRHubs();
+app.MapSTAIGrpc(app.Environment.IsDevelopment());
+
+app.MapControllers();
+
+app.Run();
---
+++ src/STAIExtensions/Example Projects/Examples.Host.ApiFull/Services/ServiceRunner.cs
@@ -0,0 +1,41 @@
+﻿using STAIExtensions.Abstractions.Data;
+using STAIExtensions.Data.AzureDataExplorer;
+using STAIExtensions.Default.DataSets.Options;
+
+namespace Examples.Host.ApiFull.Services;
+
+
+public class ServiceRunner : IHostedService
+{
+    private IDataSet _ds;
+    private readonly string _apiKey;
+    private readonly string _appId;
+
+    public ServiceRunner(IConfiguration configuration)
+    {
+        _apiKey = configuration["ReadTelemetryApiKey"];
+        _appId = configuration["ReadTelemetryAppId"];
+    }
+    
+    public Task StartAsync(CancellationToken cancellationToken)
+    {
+        // 1) Create a new telemetry loader instance
+        var telemetryLoader = new STAIExtensions.Data.AzureDataExplorer.TelemetryLoader(
+            new TelemetryLoaderOptions(_apiKey, _appId));
+        
+        // 2) Register a new dataset with the telemetry loader
+        _ds = new STAIExtensions.Default.DataSets.DataContractDataSet(telemetryLoader, new DataContractDataSetOptions(),
+            "DataContractDataSet");
+        
+        // 3) Start the auto refresh on the dataset.
+        _ds.StartAutoRefresh(TimeSpan.FromSeconds(30), cancellationToken);
+        
+        return Task.CompletedTask;
+    }
+
+    public Task StopAsync(CancellationToken cancellationToken)
+    {
+        _ds.StopAutoRefresh();
+        return Task.CompletedTask;
+    }
+}
