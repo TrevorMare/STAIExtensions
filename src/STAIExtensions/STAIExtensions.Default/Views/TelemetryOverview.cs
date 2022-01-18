4f62a39df34493858c64910a1d4159ac57450d51
Trevor Mare <trevorm336dev@gmail.com>
2022-01-18 18:29:51 +0200

    Started with Angular UI Project

---
+++ src/STAIExtensions/STAIExtensions.Default/Views/TelemetryOverview.cs
@@ -0,0 +1,70 @@
+﻿using STAIExtensions.Abstractions.Data;
+using STAIExtensions.Core.Views;
+using STAIExtensions.Default.DataSets;
+
+namespace STAIExtensions.Default.Views;
+
+public class TelemetryOverview : DataSetView
+{
+
+    #region Properties
+
+    public int? AvailabilityCount { get; private set; }
+    
+    public int? BrowserTimingsCount { get; private set; }
+    
+    public int? CustomEventsCount { get; private set; }
+    
+    public int? CustomMetricsCount { get; private set; }
+    
+    public int? DependenciesCount { get; private set; }
+    
+    public int? PageViewsCount { get; private set; }
+    
+    public int? PerformanceCountersCount { get; private set; }
+    
+    public int? RequestsCount { get; private set; }
+    
+    public int? TracesCount { get; private set; }
+    #endregion
+
+    #region Overrides
+    
+    protected override Task BuildViewData(IDataSet dataSet)
+    {
+        if (dataSet is DataContractDataSet dataContractDataSet)
+        {
+            this.LoadTotals(dataContractDataSet);
+        }
+        return Task.CompletedTask;
+    }
+    #endregion
+
+    #region Private Medhods
+
+    private void LoadTotals(DataContractDataSet dataContractDataSet)
+    {
+        this.AvailabilityCount = dataContractDataSet.Availability.Count;
+        this.BrowserTimingsCount = dataContractDataSet.BrowserTiming.Count;
+        this.CustomEventsCount = dataContractDataSet.CustomEvents.Count;
+        this.CustomMetricsCount = dataContractDataSet.CustomMetrics.Count;
+        this.DependenciesCount = dataContractDataSet.Dependencies.Count;
+        this.PageViewsCount = dataContractDataSet.PageViews.Count;
+        this.PerformanceCountersCount = dataContractDataSet.PerformanceCounters.Count;
+        this.RequestsCount = dataContractDataSet.Requests.Count;
+        this.TracesCount = dataContractDataSet.Traces.Count;
+    }
+    
+
+    private void LoadDistinctRoleNames(DataContractDataSet dataContractDataSet)
+    {
+        var result = new List<string>();
+        
+        from q in dataContractDataSet.Availability
+
+    }
+
+    #endregion
+    
+    
+}
